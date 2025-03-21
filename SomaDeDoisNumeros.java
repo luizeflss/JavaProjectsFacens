3f783424fede080ae059688852d0294a9b23f6a4
Luiz Eduardo <luizeduardu234@gmail.com>
2025-03-20 21:45:43 -0300

    Update SomaDeDoisNumeros.java

--- SomaDeDoisNumeros.java
+++ SomaDeDoisNumeros.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class SomaDeDoisNumeros{
     public static void main(String [] args) {
 
-        //defina uma variavel do tipo scanner e incialize as variaveis
+        //defina uma variavel do tipo "Scanner" e incialize as variaveis
         Scanner scn = new Scanner(System.in);
         int primeiroValor = 0;
         int segundoValor = 0;
-
+        int soma = 0;
+        
         //escreva os inputs para o usuario entregar os dados
         System.out.println("Soma de dois numeros ");
         System.out.println("Digite o primeiro numero: ");
@@ -16,7 +17,7 @@ public class SomaDeDoisNumeros{
             segundoValor = scn.nextInt();
         
         //crie uma variavel para o resultado e, faça a operação com os dados recebidos
-        int soma = primeiroValor + segundoValor;
+        soma = primeiroValor + segundoValor;
         System.out.println("O resultado da soma desses dois números é: " + soma);
 
     }
