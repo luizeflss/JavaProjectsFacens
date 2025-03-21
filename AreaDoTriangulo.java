14d6d103552606176ae31ccdd97d8c636da00be3
Luiz Eduardo <luizeduardu234@gmail.com>
2025-03-21 20:03:48 -0300

    Update AreaDoTriangulo.java

--- AreaDoTriangulo.java
+++ AreaDoTriangulo.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 public class AreaDoTriangulo {
     public static void main(String [] args) {
         Scanner scn = new Scanner(System.in);
-
             int base = 0;
             int altura = 0;
             int area = (base*altura) / 2;
     
-    System.out.println("Digite o valor da base: ");
-        base = scn.nextInt();
-    System.out.println("Digite o valor da altura: ");
-        altura = scn.nextInt();
+        System.out.println("Digite o valor da base: ");
+            base = scn.nextInt();
+        System.out.println("Digite o valor da altura: ");
+            altura = scn.nextInt();
 
-    area = (base*altura) / 2;
-    System.out.println("O valor da area é de: " + area);
+        area = (base*altura) / 2;
+        System.out.println("O valor da area é de: " + area);
 
     }
 }
