d3980cb426d406568e7c03f75d8f89a51eb9d534
Luiz Eduardo <luizeduardu234@gmail.com>
2025-03-29 12:56:28 -0300

    Update AreaDoCirculo.java

--- AreaDoCirculo.java
+++ AreaDoCirculo.java
@@ -4,13 +4,13 @@ public class AreaDoCirculo {
 
     public static void main(String [] args){
         Scanner scn = new Scanner(System.in);
-            int raio = 0;
-            double pi = 3.14;
-            double resultado = pi * (raio*raio);
+        int raio = 0;
+        double pi = 3.14;
+        double resultado = pi * (raio*raio);
 
         System.out.println("Digite o valor do raio: ");
-            raio = scn.nextInt();
-            resultado = pi * (raio*raio);
+        raio = scn.nextInt();
+        resultado = pi * (raio*raio);
 
         System.out.println("O valor da circunferencia é: " + resultado);  
 
