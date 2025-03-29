9911ea266962287111b4bf6bb3fe68cb2521a800
Luiz Eduardo <luizeduardu234@gmail.com>
2025-03-29 18:20:06 -0300

    Add files via upload

---
+++ AC1.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class AC1 {
+    public static void main(String[] args) {
+        //inicialize as variaveis
+        Scanner scn = new Scanner(System.in);
+        double ac1 = 0.0;
+        double ac2 = 0.0;
+        double ag = 0.0;
+        double af = 0.0;
+        double notaminima = 0.0;
+
+        //receba as notas do usuario
+        System.out.println("Digite sua nota na AC1: ");
+        ac1 = scn.nextDouble();
+        System.out.println("Digite sua nota na AC2: ");
+        ac2 = scn.nextDouble();
+        System.out.println("Digite sua nota na AG: ");
+        ag = scn.nextDouble();
+        System.out.println("Digite sua nota na AF: ");
+        af = scn.nextDouble();
+
+        //recaba a nota minima para aprovacao
+        System.out.println("Digite a nota mínima para aprovação: ");
+        notaminima = scn.nextDouble();
+
+        //calcule a media final
+        double mediafinal = (ac1 * 0.15) + (ac2 *0.30) + (ag * 0.10) + (af * 0.45);
+
+        //mostre as notas de cada avaliacao
+        System.out.println("AC1: " + ac1);
+        System.out.println("AC2: " + ac2);
+        System.out.println("AG: " + ag);
+        System.out.println("AF: " + af);
+
+        //mostre a media final
+        System.out.println("Sua média final é: " + mediafinal);
+
+        //mostre se foi aprovado ou nao
+        if(mediafinal >= notaminima){
+            System.out.println("Aprovado!");
+        }else{
+            System.out.println("Reprovado!");
+        }
+
+        scn.close();
+    }
+}
