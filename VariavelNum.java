2848b162f7fce2106b83e31d70dcd7d0ce860b94
Luiz Eduardo <luizeduardu234@gmail.com>
2025-03-29 12:55:55 -0300

    Update VariavelNum.java

--- VariavelNum.java
+++ VariavelNum.java
@@ -1,10 +1,8 @@
 public class VariavelNum {
     public static void main (String [] args){
-        int num = 423;
-        num = num * num;
-
-        System.out.println( num ) ;
+    int num = 423;
+    num = num * num;
+    System.out.println( num ) ;
         
     }
-
 }
